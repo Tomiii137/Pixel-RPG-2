906482aa2ea7f1cfa99be9ed25fcace04ff18525
Tomiii137 <60294017+Tomiii137@users.noreply.github.com>
2020-01-25 21:41:12 +0100

    #1 movement implemented

---
+++ New Unity Project/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 3.1f;
+
+
+    float horizontalInput;
+    float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // movement input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //rotation input
+        
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //movement
+        Vector2 movement = new Vector2(horizontalInput, verticalInput) * speed * Time.fixedDeltaTime;
+        rb.AddRelativeForce(movement);
+    }
+
+
+}
