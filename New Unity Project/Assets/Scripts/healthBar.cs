0741b5f0710678295cf20699503e2f153ef58d08
Tomiii137 <60294017+Tomiii137@users.noreply.github.com>
2020-01-26 12:43:30 +0100

    healtbar

---
+++ New Unity Project/Assets/Scripts/healthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{
+    [SerializeField] float currentLifepoints;
+    [SerializeField] float maxLifepoints;
+    public Image healthBarImage;
+    public Text ratioText;
+    void Start()
+    {
+        updateLifeBar();
+    }
+
+    private void updateLifeBar()
+    {
+        float ratio = currentLifepoints / maxLifepoints;
+        healthBarImage.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString() + '%';
+    }
+    void Update()
+    {
+        updateLifeBar();
+    }
+
+}
