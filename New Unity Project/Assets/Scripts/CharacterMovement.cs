c2488f09eaf436cd4bd51e758dc21cf4f835b3ad
xLegend27x <alpha.kevin4253@gmail.com>
2020-01-26 18:35:44 +0100

    Playa animations

--- New Unity Project/Assets/Scripts/CharacterMovement.cs
+++ New Unity Project/Assets/Scripts/CharacterMovement.cs
@@ -6,10 +6,11 @@ public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Camera playerCam;
+    [SerializeField] GameObject gun;
     [SerializeField] float speed = 3.1f;
 
-
     Vector2 movement;
+    public Vector2 lookDirection;
 
     Vector2 mousePos;
     // Start is called before the first frame update
@@ -36,9 +37,11 @@ public class CharacterMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
 
         //rotation
-        Vector2 lookDirection = mousePos - rb.position;
+        lookDirection = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+
+        Quaternion rotates = Quaternion.Euler(0, 0, angle);
+        gun.transform.rotation = rotates;
     }
 
 
